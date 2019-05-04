aa0227cb5aafc0c7b6793f90925b9a8d225c3dca
Gabriel Lisboa <lisbiel@gmail.com>
2019-05-04 12:47:43 -0300

    com os botões codigo+class pessoa

--- app/src/main/java/com/example/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hackathon/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button souGuia = findViewById(R.id.sou_guia);
+
+        souGuia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, NovaTela.class);
+                startActivity(it);
+            }
+        });
+        Button estouperdido = findViewById(R.id.estou_perdido);
+
+        souGuia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, NovaTela.class);
+                startActivity(it);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hackathon/PessoaPerdida.java
@@ -0,0 +1,7 @@
+package com.example.hackathon;
+
+public class PessoaPerdida {
+    private String nome;
+    private String partida;
+    private String Chegada;
+}
